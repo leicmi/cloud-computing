5f9b49ef962469fc34d051d543250d52f15cc0e0
Michael Leichtfried <m.leichtfried@student.tudelft.nl>
2020-06-19 13:07:35 +0200

    Add initial monitoring implementation

--- AWS Lambdas/ImageMagickSrc/index.js
+++ AWS Lambdas/ImageMagickSrc/index.js
@@ -8,6 +8,13 @@ const s3Util = require('./s3-util'),
 	OUTPUT_BUCKET = process.env.OUTPUT_BUCKET,
 	MIME_TYPE =  process.env.MIME_TYPE;
 
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.DynamoDBTable;
+
 exports.handler = function (eventObject, context) {
 	const eventRecord = eventObject.Records && eventObject.Records[0],
 		inputBucket = eventRecord.s3.bucket.name,
@@ -18,13 +25,31 @@ exports.handler = function (eventObject, context) {
 		inputFile = path.join(workdir,  id + path.extname(key)),
 		outputFile = path.join(workdir, 'converted-' + id + EXTENSION);
 
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+	var params = {TableName : tableName, Item: { id : key, jobStatus: "CONVERTING" }};
+	docClient.put(params, function(err, data) {
+		if (err) console.log(err);
+		//else console.log(data);
+	  });
 
 	console.log('converting', inputBucket, key, 'using', inputFile);
-	return s3Util.downloadFileFromS3(inputBucket, key, inputFile)
-		.then(() => childProcessPromise.spawn(
-			'/opt/bin/convert',
-			[inputFile, '-resize', `${THUMB_WIDTH}x`, outputFile],
-			{env: process.env, cwd: workdir}
-		))
-		.then(() => s3Util.uploadFileToS3(OUTPUT_BUCKET, resultKey, outputFile, MIME_TYPE));
+
+	result = s3Util.downloadFileFromS3(inputBucket, key, inputFile)
+	.then(() => childProcessPromise.spawn(
+		'/opt/bin/convert',
+		[inputFile, '-resize', `${THUMB_WIDTH}x`, outputFile],
+		{env: process.env, cwd: workdir}
+	))
+	.then(() => s3Util.uploadFileToS3(OUTPUT_BUCKET, resultKey, outputFile, MIME_TYPE));
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+	var params = {TableName : tableName, Item: { id : key, jobStatus: "FINISHED" }};
+	docClient.put(params, function(err, data) {
+		if (err) console.log(err);
+		//else console.log(data);
+	  });
+
+	return result
 };
